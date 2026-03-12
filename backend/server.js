971ed404c31d6b5accb5dad620eee1f230c4619e
Santhi-ya <santhiyasr2005@gamil.com>
2026-03-13 00:17:00 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -17,5 +17,5 @@ mongoose.connect(process.env.MONGO_URI);
 app.use("/api/sensors",sensorRoutes);
 
 app.listen(PORT,()=>{
-  console.log("Server running on port 5000");
+  console.log(`Server running on port ${PORT}`);
 });
