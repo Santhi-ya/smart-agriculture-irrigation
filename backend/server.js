a162cbcf172a40161c7b0c8ed38ba73944d9043f
Santhi-ya <santhiyasr2005@gamil.com>
2026-05-21 13:12:46 +0530

    updated the project successfully

--- backend/server.js
+++ backend/server.js
@@ -8,14 +8,30 @@ const sensorRoutes = require("./routes/sensorRoutes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI);
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected Successfully");
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection Error:");
+    console.log(err);
+  });
 
+// Routes
+app.use("/api/sensors", sensorRoutes);
 
-app.use("/api/sensors",sensorRoutes);
+// Default Route
+app.get("/", (req, res) => {
+  res.send("Smart Agriculture Backend Running");
+});
 
-app.listen(PORT,()=>{
+// Server Start
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
